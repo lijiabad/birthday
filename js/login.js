d24a4795e126b43debe505ecfa491740259fefa2
ljb <1098420639@qq.com>
2022-11-22 17:00:25 +0800

    添加文本/修改边界

--- js/login.js
+++ js/login.js
@@ -20,10 +20,53 @@ $('#login-button').click(function (event) {
       location.href = "BirthdayCake.html";
     }, 2000);
   }
+  else if (userName == "1" && pwd == "1") {
+    event.preventDefault();
+    $('form').fadeOut(500);
+    $('.wrapper').addClass('form-success');
+    requestFullScreen();
+    setTimeout(function () {
+      location.href = "BirthdayCake.html";
+    }, 2000);
+  }
   else {
     alert("密码错误");
   }
+  // handleLogin();
 });
+
+//新增 js变量获取函数
+function handleLogin() {
+  var e = $("#userName").val();
+  var p = $("#password").val();
+
+  if(e != "") {
+      $.ajax({ 
+               type: 'POST', 
+               url: 'http://localhost/BirthdayCake.html', 
+               crossDomain: true,
+               data:  {username: e},
+               dataType: 'json', 
+               async: false,
+
+               success: function (response){ 
+                  if (response.success) { 
+                      $.mobile.changePage("#loginTest");
+                   } 
+                  else {
+                      alert("Your login failed");
+                  }
+               },
+               error: function(error){
+                  alert('Could not connect to the database' + error);
+               }
+             }); 
+  }
+  else {
+      alert("You must enter username and password");
+  }
+  return false;
+}
 
 function requestFullScreen (element) {
   var element = document.documentElement;
